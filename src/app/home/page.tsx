f521ac5dd10bee499016b706dc718ce81ed3ab0f
bengeendokter <benifit.arts@gmail.com>
2023-06-06 11:32:55 +0200

    Log QrCodes scan data to console

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,6 +3,7 @@ import QRCode from 'react-qr-code';
 import styles from './home.module.css';
 import { Link, useSearchParams } from 'react-router-dom';
 import { DragDropContext, Draggable, Droppable, OnDragEndResponder } from 'react-beautiful-dnd';
+import QrScanner from "qr-scanner";
 
 const defaultCountries = ["Belgium", 'Sweden', 'Finland'];
 
@@ -11,6 +12,33 @@ function Home()
     const [queryParams, setQueryParams] = useSearchParams();
     const [inputValue, setInputValue] = React.useState("");
     const [countries, setCountries] = React.useState<string[]>([]);
+    const videoRef = React.useRef<HTMLVideoElement>(null);
+    const [scanner, setScanner] = React.useState<QrScanner>();
+    const [scannerRunning, setScannerRunning] = React.useState(false);
+
+    React.useEffect(() =>
+    {
+        if(videoRef.current === null)
+        {
+            return;
+        }
+
+        return setScanner(new QrScanner(videoRef.current, result => console.log(result.data), {}));
+    }, [setScanner]);
+
+    const toggleScanner = React.useCallback(() =>
+    {
+        if(scannerRunning)
+        {
+            scanner?.stop();
+        }
+        else
+        {
+            scanner?.start()
+        }
+        setScannerRunning(!scannerRunning);
+    }, [scanner, scannerRunning]);
+
 
     React.useEffect(() =>
     {
@@ -54,68 +82,75 @@ function Home()
         setQueryParams(queryParams);
         setInputValue(JSON.stringify(defaultCountries));
         setCountries(defaultCountries);
-}, [queryParams, setQueryParams]);
+    }, [queryParams, setQueryParams]);
 
-const onDragEnd: OnDragEndResponder = (result) =>
-{
-    const { destination, source, draggableId } = result;
-    if(!destination)
+    const onDragEnd: OnDragEndResponder = (result) =>
     {
-        return;
-    }
+        const { destination, source, draggableId } = result;
+        if(!destination)
+        {
+            return;
+        }
 
-    if(destination.droppableId === source.droppableId && destination.index === source.index)
+        if(destination.droppableId === source.droppableId && destination.index === source.index)
+        {
+            return;
+        }
+
+        const column = Array.from(countries);
+        column.splice(source.index, 1);
+        column.splice(destination.index, 0, draggableId);
+
+        setCountries(column);
+        updateInputValue(JSON.stringify(column));
+    };
+
+    const handleToggleScanner = React.useCallback(() =>
     {
-        return;
-    }
+        toggleScanner();
+    }, [toggleScanner]);
 
-    const column = Array.from(countries);
-    column.splice(source.index, 1);
-    column.splice(destination.index, 0, draggableId);
-
-    setCountries(column);
-    updateInputValue(JSON.stringify(column));
-};
-
-return (
-    <main className={styles.main}>
-        <ul>
-            <li>
-                <Link to="/">Home</Link>
-            </li>
-            <li>
-                <Link to="/about">About</Link>
-            </li>
-        </ul>
-        <QRCode value={`http://localhost:3000/?inputValue=${inputValue}`}></QRCode>
-        <p>http://localhost:3000/?inputValue={inputValue}</p>
-        <div>
-            <DragDropContext onDragEnd={onDragEnd}>
-                <Droppable droppableId='countries'>
-                    {
-                        (provided) => (<div ref={provided.innerRef} {...provided.droppableProps} >
-                            {countries.map((country, index) => <Draggable draggableId={country} index={index} key={country}>
-                                {(provided, snapshot) =>
-                                (
-                                    <div
-                                        {...provided.draggableProps}
-                                        {...provided.dragHandleProps}
-                                        ref={provided.innerRef} >
-                                        <p
-                                            style={{ backgroundColor: snapshot.isDragging ? "red" : "green" }}>
-                                            {country}</p>
-                                    </div>
-                                )
-                                }
-                            </Draggable>)}
-                            {provided.placeholder}
-                        </div>)
-                    }
-                </Droppable>
-            </DragDropContext>
-        </div>
-        <button onClick={handleReset}>Reset</button>
-    </main>)
+    return (
+        <main className={styles.main}>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+            </ul>
+            <video ref={videoRef} ></video>
+            <QRCode value={`http://localhost:3000/?inputValue=${inputValue}`}></QRCode>
+            <p>http://localhost:3000/?inputValue={inputValue}</p>
+            <div>
+                <DragDropContext onDragEnd={onDragEnd}>
+                    <Droppable droppableId='countries'>
+                        {
+                            (provided) => (<div ref={provided.innerRef} {...provided.droppableProps} >
+                                {countries.map((country, index) => <Draggable draggableId={country} index={index} key={country}>
+                                    {(provided, snapshot) =>
+                                    (
+                                        <div
+                                            {...provided.draggableProps}
+                                            {...provided.dragHandleProps}
+                                            ref={provided.innerRef} >
+                                            <p
+                                                style={{ backgroundColor: snapshot.isDragging ? "red" : "green" }}>
+                                                {country}</p>
+                                        </div>
+                                    )
+                                    }
+                                </Draggable>)}
+                                {provided.placeholder}
+                            </div>)
+                        }
+                    </Droppable>
+                </DragDropContext>
+            </div>
+            <button onClick={handleReset}>Reset</button>
+            <button onClick={handleToggleScanner}>Toggle Scanner</button>
+        </main>)
 }
 
 export default Home;
